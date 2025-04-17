709ba37e016b0adc1e0711011a6d4dcbd5a15d1d
pekoonekoo <ardi99e@gmail.com>
2025-04-17 11:39:49 +0700

    Memperbaiki atau menyamakan Coment pada code sebelumnya dari bahasa indonesia ke inggris semua

--- TaxCalculator-SCD/TaxCalculator/src/lib/Employee.java
+++ TaxCalculator-SCD/TaxCalculator/src/lib/Employee.java
@@ -36,10 +36,10 @@ public class Employee {
 	}
 	
 	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
+ * Function to determine the monthly salary of an employee based on their grade (grade 1: 3,000,000 per month, grade 2: 5,000,000 per month, grade 3: 7,000,000 per month)
+ * If the employee is a foreigner, the monthly salary is increased by 50%
+ 	*/
+
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
